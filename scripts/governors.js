791dfe884eca022cf94c2f396c195d3da0dd6770
Kris Cagle <Kriscagle@gmail.com>
2026-03-17 13:19:29 -0500

    added governors.js and changes to main

--- scripts/governors.js
+++ scripts/governors.js
@@ -1,24 +1,24 @@
-export const addWheelListener = (changeEvent) => {
+export const addGovernorListener = (changeEvent) => {
     if (changeEvent.target.id === "governor") {
         const chosenOption = parseInt(changeEvent.target.value)
-        setWheelChoice(chosenOption)
+        setGovernorChoice(chosenOption)
     }
 }
-document.addEventListener("change", addWheelListener)
+document.addEventListener("change", addGovernorListener)
 
-export const Governors = async () => {
-    const response = await fetch("http://localhost:8088/wheels")
-    const wheels = await response.json()
+export const renderGovernors = async () => {
+    const response = await fetch("http://localhost:8088/governors")
+    const governor = await response.json()
 
-    const wheelDropDown = wheels.map((wheel) => {
-        return `<option value="${wheel.id}">${wheel.size}</option>`
+    const governorDropDown = renderGovernors.map((governor) => {
+        return `<option value="${governor.id}">${governor.name}</option>`
     })
 
     return `
         <div>
             <select id="wheel">
                 <option value="0">Select a set of wheels...</option>
-                ${wheelDropDown.join("")}
+                ${governorDropDown.join("")}
             </select>
         </div>
     `
