272fa12f7403e1ed67de8c1021bbcd4573742efe
Austin <cookaustin46@gmail.com>
2026-03-18 13:16:21 -0500

    fixed main comments

--- scripts/main.js
+++ scripts/main.js
@@ -1,58 +1,50 @@
-//import { renderGovernors } from "./governors.js"
+import { renderGovernors } from "./governors.js"
 import { renderFacilities, renderFacilityInventory } from "./facilities.js"
-//import { renderSpaceCart, spaceCartButton } from "./spaceCart.js"
-//import { renderColonyInventory } from "./colonyInventory.js"
+import { renderSpaceCart, spaceCartButton } from "./spaceCart.js"
+import { renderColonyInventory } from "./colonyInventory.js"
 
 
 const container = document.querySelector("#container")
 
 const render = async () => {
-//    const governorsHTML = await renderGovernors()
+   const governorsHTML = await renderGovernors()
    const facilitiesHTML = await renderFacilities()
    const facilityInventoryHTML = await renderFacilityInventory()
-//    const colonyInventoryHTML = /*await*/ renderColonyInventory()
-//    const spaceCartHTML = /*await*/ renderSpaceCart()
+   const colonyInventoryHTML = /*await*/ renderColonyInventory()
+   const spaceCartHTML = /*await*/ renderSpaceCart()
 
 
 
 
-    const composedHTML = `
-        <section class="dropdown_facility_choices">
-              <h2>Choose a facility</h2>
-              ${facilitiesHTML}
-        </section>
-        <section class="facility_inventory">
-            ${facilityInventoryHTML}
-        </section>
+    const composedHTML =
     `
-//     `
-//         <h1>Solar System Mining Marketplace</h1>
-//         <article class="dropdown_menus">
-//             <section class="dropdown_governor_choices">
-//                 <h2>Choose a governor</h2>
-//                 ${governorsHTML}
-//             </section>
-//          <section class="dropdown_facility_choices">
-//              <h2>Choose a facility</h2>
-//              ${facilitiesHTML}
-//          </section>
-//      </article>
+        <h1>Solar System Mining Marketplace</h1>
+        <article class="dropdown_menus">
+            <section class="dropdown_governor_choices">
+                <h2>Choose a governor</h2>
+                ${governorsHTML}
+            </section>
+         <section class="dropdown_facility_choices">
+             <h2>Choose a facility</h2>
+             ${facilitiesHTML}
+         </section>
+     </article>
         
-//      <section class="radio_facility_inventory">
-//          <h2>Facility Minerals</h2>
-//          ${colonyInventoryHTML}
-//      </section>
+     <section class="radio_facility_inventory">
+         <h2>Facility Minerals</h2>
+         ${colonyInventoryHTML}
+     </section>
         
 
-//      <article class="order">
-//          ${spaceCartButton}
-//      </article>
+     <article class="order">
+         ${spaceCartButton}
+     </article>
 
-//      <article class="customOrders">
-//          <h2>Space Cart</h2>
-//          ${spaceCartHTML}
-//      </article>
-//  `
+     <article class="customOrders">
+         <h2>Space Cart</h2>
+         ${spaceCartHTML}
+     </article>
+ `
     container.innerHTML = composedHTML
 }
 
