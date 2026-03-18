fad358110523893a9368e6cf3a2b3a16e0afdc01
DevpaulChani <devpaulc@gmail.com>
2026-03-18 11:34:22 -0500

    more async

--- scripts/colonyInventory.js
+++ scripts/colonyInventory.js
@@ -2,19 +2,25 @@ import database from "./database.js"
 import { getSelectedGovernorId } from "./TransientState.js"
  
 export const renderColonyInventory = async () => {
+
     if (!getSelectedGovernorId()) {
         return <div class="select">Select a governor to view colony inventory</div>
     }
     
     const governorResponse = await fetch("http://localhost:8088/governors")
     const governors = await governorResponse.json()
+    const coloniesResponse = await fetch("http://localhost:8088/colonies")
+    const colonies = await coloniesResponse.json()
+    const colonyMineralsResponse = await fetch("http://localhost:8088/colonyMinerals")
+    const colonyMineral = await colonyMineralsResponse.json()
+    const mineralsResponse = await fetch("http://localhost:8088/minerals")
+    const minerals = await mineralsResponse.json()
+
     const governor = governors.find(gArray => gArray.id === getSelectedGovernorId())
     if (!governor) {
         return <div class="error">Governor not found</div>
     }
     
-    const coloniesResponse = await fetch("http://localhost:8088/colonies")
-    const colonies = await coloniesResponse.json()
     const colonyId = governor.colonyId
     const colony = colonies.find(cArray => cArray.id === colonyId)
     let colonyName
@@ -24,10 +30,6 @@ export const renderColonyInventory = async () => {
         colonyName = "Unknown"
     }
     
-    const colonyMineralsResponse = await fetch("http://localhost:8088/colonies")
-    const colonyMineral = await colonyMineralsResponse.json()
-    const mineralsResponse = await fetch("http://localhost:8088/minerals")
-    const minerals = await mineralsResponse.json()
     const colonyMinerals = colonyMineral.filter(cmArray => cmArray.colonyId === colonyId)
     
     if (colonyMinerals.length === 0) {
