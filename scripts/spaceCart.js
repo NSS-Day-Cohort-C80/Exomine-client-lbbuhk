9b0ea99895a4f934e10d35caca57c354232b14b9
DevpaulChani <devpaulc@gmail.com>
2026-03-19 09:20:49 -0500

    initial

--- scripts/spaceCart.js
+++ scripts/spaceCart.js
@@ -1,3 +1,11 @@
+import { getSelectedMineralId } from "./TransientState"
+
+const mineralsResponse = await fetch("http://localhost:8088/minerals")
+const minerals = await mineralsResponse.json()
+const selectedMineral = getSelectedMineralId()
+const mineral = minerals.find(mArray => mArray.id === selectedMineral.mineralId)
+export const mineralToPurchase = () => {return`<h2> ${mineral.name}</h2>`}
+
 
 
 export const spaceCartButton = () => {
