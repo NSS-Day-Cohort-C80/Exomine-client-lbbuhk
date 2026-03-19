99af6236288d718f9b4164288d4a8b2775d8d103
DevpaulChani <devpaulc@gmail.com>
2026-03-19 10:21:36 -0500

    test

--- scripts/spaceCart.js
+++ scripts/spaceCart.js
@@ -1,11 +1,18 @@
-import { getSelectedMineralId } from "./TransientState"
+import { getSelectedMineralId } from "./TransientState.js"
 
 const mineralsResponse = await fetch("http://localhost:8088/minerals")
 const minerals = await mineralsResponse.json()
+const facilitiesResponse = await fetch("http://localhost:8088/facilities")
+const facilities = await facilitiesResponse.json()
+const selectedFacility = getSelectedfacilityId()
 const selectedMineral = getSelectedMineralId()
-const mineral = minerals.find(mArray => mArray.id === selectedMineral.mineralId)
-export const mineralToPurchase = () => {return`<h2> ${mineral.name}</h2>`}
 
+
+export const mineralToPurchase = () => {
+    const mineral = minerals.find(mArray => mArray.id === selectedMineral)
+    const facility = facilities.find(fArray => fArray.id === selectedFacility)
+    return`<h2> 1 ton of ${mineral.name} from ${facility.name}</h2>`
+}
 
 
 export const spaceCartButton = () => {
@@ -17,3 +24,32 @@ export const addOrderButtonListener = () => {
         spaceCartButton()
     })
 }
+
+/**
+import { getSelectedMineralId } from "./TransientState"
+
+const mineralsResponse = await fetch("http://localhost:8088/minerals")
+const minerals = await mineralsResponse.json()
+const facilitiesResponse = await fetch("http://localhost:8088/facilities")
+const facilities = await facilitiesResponse.json()
+const selectedFacility = getSelectedfacilityId()
+const selectedMineral = getSelectedMineralId()
+
+
+export const mineralToPurchase = () => {
+    const mineral = minerals.find(mArray => mArray.id === selectedMineral)
+    const facility = facilities.find(fArray => fArray.id === selectedFacility)
+    return`<h2> 1 ton of ${mineral.name} from ${facility.name}</h2>`
+}
+
+
+export const spaceCartButton = () => {
+    return `<button id="spaceCartBtn">Purchase Mineral</button>`
+}
+
+export const addOrderButtonListener = () => {
+    document.querySelector("#spaceCartBtn").addEventListener("click", () => {
+        spaceCartButton()
+    })
+}
+ */
